93779bc54073f07f99d0466ab4c30e77811a2b69
Latchezar Nikolov <latcho.nikolov@gmail.com>
2018-10-16 09:54:38 +0300

    created spring project

---
+++ spring/src/main/java/com/property/landlordapp/LandlordappApplication.java
@@ -0,0 +1,12 @@
+package com.property.landlordapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandlordappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LandlordappApplication.class, args);
+    }
+}
