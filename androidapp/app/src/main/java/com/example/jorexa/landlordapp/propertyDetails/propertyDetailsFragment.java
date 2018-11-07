95d08f122c9fbc5ba75040b8ecec54a4a7d1bdaa
Georvi Vlasakiev <g.vlasakiev@gmail.com>
2018-11-07 03:31:39 +0200

    Added Property Name, Address and Price in fragment_property_details.xml

--- androidapp/app/src/main/java/com/example/jorexa/landlordapp/propertyDetails/propertyDetailsFragment.java
+++ androidapp/app/src/main/java/com/example/jorexa/landlordapp/propertyDetails/propertyDetailsFragment.java
@@ -6,6 +6,8 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.jorexa.landlordapp.R;
 import com.example.jorexa.landlordapp.models.Property;
@@ -13,10 +15,22 @@ import com.example.jorexa.landlordapp.userprofile.UserProfileContracts;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class propertyDetailsFragment extends Fragment implements propertyDetailsContracts.View {
+
+    @BindView(R.id.tv_property_details_name)
+    TextView mPropertyName;
+
+    @BindView(R.id.tv_property_details_address)
+    TextView mPropertyAddress;
+
+    @BindView(R.id.tv_property_details_price)
+    TextView mPropertyPrice;
 
     private propertyDetailsContracts.Presenter mPresenter;
 
@@ -34,14 +48,20 @@ public class propertyDetailsFragment extends Fragment implements propertyDetails
 
     @Override
     public void setPropertyInformation(Property property) {
-        int a = 5;
+        mPropertyName.setText(property.getPropertyName());
+        mPropertyAddress.setText(property.getAddress());
+        mPropertyPrice.setText(property.getPropertyPrice()+"");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_property_details, container, false);
+        View view = inflater.inflate(R.layout.fragment_property_details, container, false);
+
+        ButterKnife.bind(this, view);
+
+        return view;
     }
 
     @Override
