6b78cc17a0176b2faf85de638c3dbf991170b37a
Georvi Vlasakiev <g.vlasakiev@gmail.com>
2018-11-07 15:16:02 +0200

    Added chatListAdapter in chat pachage

---
+++ androidapp/app/src/main/java/com/example/jorexa/landlordapp/chat/chatListAdapter.java
@@ -0,0 +1,47 @@
+package com.example.jorexa.landlordapp.chat;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.jorexa.landlordapp.R;
+import com.example.jorexa.landlordapp.models.Property;
+
+import javax.inject.Inject;
+
+public class chatListAdapter extends ArrayAdapter<Property> {
+    @Inject
+    public chatListAdapter(@NonNull Context context) {
+        super(context, -1);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater layoutInflater =
+                (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View view = layoutInflater.inflate(
+                R.layout.property_item,
+                parent,
+                false
+        );
+
+        TextView nameTextView = view.findViewById(R.id.tv_property_name);
+        //TextView addressTextView = view.findViewById(R.id.tv_property_addresss);
+
+        Property property = getItem(position);
+
+        nameTextView.setText(property.getPropertyName());
+        //addressTextView.setText(property.getAddress());
+
+        return view;
+    }
+}
+
+
--- androidapp/app/src/main/java/com/example/jorexa/landlordapp/diconfig/ViewsModule.java
+++ androidapp/app/src/main/java/com/example/jorexa/landlordapp/diconfig/ViewsModule.java
@@ -3,6 +3,8 @@ package com.example.jorexa.landlordapp.diconfig;
 import android.content.Context;
 import android.widget.ArrayAdapter;
 
+import com.example.jorexa.landlordapp.chat.chatListAdapter;
+import com.example.jorexa.landlordapp.models.LoginUser;
 import com.example.jorexa.landlordapp.models.Property;
 import com.example.jorexa.landlordapp.userprofile.PropertiesListAdapter;
 
@@ -14,5 +16,11 @@ public class ViewsModule {
     @Provides
     public ArrayAdapter<Property> propertyArrayAdapter(Context context) {
         return new PropertiesListAdapter(context);
+    }
+
+    @Provides
+    public ArrayAdapter<LoginUser> chatArrayAdapter(Context context) {
+        //return new chatListAdapter(context);
+        return null;
     }
 }
